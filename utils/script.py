79cc49f25e6129f199b42778cd897b66f7050adc
THE-HYPER-NEW <Animechillax728@gmail.com>
2025-03-13 18:03:56 +0530

    Update script.py

--- utils/script.py
+++ utils/script.py
@@ -1,21 +1,25 @@
 class script(object):
-    START = """**Hello {} 👾**
+    START = """**Hello {} 🥀**
    
 I am **Find Post Bot**.I am best Channel Link Search Bot! 
 I Will filter your channel posts automatically and send it in your group chat when Someone search it."""
 
-    HELP = """To Use me In A Group
+    HELP = """<b>To Use me In A Group
 
 - Add me in your group & channel with all permissions. 
+
 - Send /verify in group & wait for It To Accept Or Directly Contact To Owner After Request @THEHYPER_ACX.
+
 - After verification send /connect YourChannelID
+
 - Example : /connect -100xxxxxxxxxx
+
 - Done ✅. <b><I>Enjoy 💜❤</I></b> )
 
 Remove a Channel with - /disconnect-100xxxxxxxxxxx
 this will help you to remove a indexed channel from your group.
 
-Get connected channels list with - /connections"""
+Get connected channels list with - /connections</b>"""
 
     ABOUT = """Developed By @THEHYPER_ACX 
 
